476cd17360b069aef5c102706281f3580f259c25
Ermias Tsegu <yetabalij@gmail.com>
2026-06-29 15:56:23 +0300

    commented code removed.

--- backend/app/tests/test_metadata_catalog.py
+++ backend/app/tests/test_metadata_catalog.py
@@ -1,11 +1,3 @@
 from app.database.metadata_catalog import save_catalog
-#from app.database.metadata_extractor import get_columns, get_tables
-
-#for table in get_tables():
-#    print(table)    
-
-#print(get_columns("accounts")) 
-
-
 
 save_catalog()
